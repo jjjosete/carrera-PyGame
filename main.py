6b0169ded6a3da2dc2f10a2885d762a94ab4dcf9
jjjosete <jfernan0001@gmail.com>
2021-04-19 18:01:38 +0200

    primer juego con pygame y imagenes del juego

---
+++ main.py
@@ -0,0 +1,44 @@
+import pygame
+import sys
+
+class Game():
+    
+    corredores = []
+    
+    def __init__(self):
+        
+        self.__screen = pygame.display.set_mode((640, 480))
+        pygame.display.set_caption('Carrera de bichos')
+        self.background = pygame.image.load("images/background.png")
+        
+        self.runner = pygame.image.load("images/cosica.png")
+        
+
+    def competir(self):
+        
+        x = 0
+        hayGanador = False
+        
+        while not hayGanador:
+            #comprobacion de los eventos
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+            #refrescar la pantalla
+            self.__screen.blit(self.background, (0,0))
+            self.__screen.blit(self.runner, (x, 240))
+            pygame.display.flip()
+            
+            x +=1
+            if x >= 250:
+                hayGanador = True
+                
+        pygame.quit()
+        sys.exit()
+
+if __name__ =='__main__':
+    pygame.init()
+    game = Game()
+    game.competir()
